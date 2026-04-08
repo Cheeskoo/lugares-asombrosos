8999b44e5c5b6dd61c8826609e510b642fb381f2
Cheeskoo <159967078+Cheeskoo@users.noreply.github.com>
2026-04-08 11:26:26 -0400

    up
    Agregue las imagenes que enviara la api al front, añadi un archivo json con las rutas, titulos y descripciones de las mismas

---
+++ server.js
@@ -0,0 +1,26 @@
+import http from 'node:http';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const PORT = 3000;
+
+const __dirname = import.meta.dirname;
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        res.statusCode = 204;
+        res.end();
+        return;
+    }
+    if(req.url === '/api' && req.method === 'GET') {
+        try{
+            const jsonPath = path.join(__dirname, 'lugares.json');
+        }
+    }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}`)
+});
