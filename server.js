4abdd96492670cb67be3d2c6519dc06f906903c6
Cheeskoo <159967078+Cheeskoo@users.noreply.github.com>
2026-04-09 09:20:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,22 +5,44 @@ import path from 'node:path';
 const PORT = 3000;
 
 const __dirname = import.meta.dirname;
-const server = http.createServer((req, res) => {
+
+const server = http.createServer(async (req, res) => {
+
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+   
+    /*Pre flight request*/
     if (req.method === 'OPTIONS') {
-        res.statusCode = 204;
+        res.statusCode = 204; /*No content*/
         res.end();
         return;
     }
+
     if(req.url === '/api' && req.method === 'GET') {
         try{
             const jsonPath = path.join(__dirname, 'data.json');
-            const data = await fs.readFile(jsonPath);
-            
-        }
+            const data = await fs.readFile(jsonPath, 'utf-8');
+
+            res.writeHead(200,{
+                'Content-type': 'application/json',
+                'Charset': 'utf-8'
+            })
+            res.end(data);
+        }catch(err){
+            res.writeHead(500, {
+                'Content-type': 'application/json'});
+                res.end(JSON.stringify({
+                    error: `No se pudo leer la base de datos`
+                }))
     }
+}else {
+        res.writeHead(404,{
+            'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify({ message:'Ruta no encontrada'}))
+    }
+
 });
 
 server.listen(PORT, () => {
